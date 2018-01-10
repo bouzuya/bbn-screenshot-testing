36c89d48b594526fb9c7f4d706fe78042adc0bb1
bouzuya <m@bouzuya.net>
2018-01-10 13:00:46 +0900

    add laptop (1024x768) test

--- test/scenario/index.ts
+++ test/scenario/index.ts
@@ -12,11 +12,27 @@ const scenarios: Scenario[] = [
   },
   {
     action: async (page: Page) => {
+      await page.setViewport({ height: 768, width: 1024 });
+      await page.goto('https://blog.bouzuya.net/2018/01/01/');
+      return {};
+    },
+    name: 'entry-detail/1024x768'
+  },
+  {
+    action: async (page: Page) => {
       await page.setViewport({ height: 480, width: 320 });
       await page.goto('https://blog.bouzuya.net/2018/01/01/related/');
       return {};
     },
     name: 'entry-index/320x480'
+  },
+  {
+    action: async (page: Page) => {
+      await page.setViewport({ height: 768, width: 1024 });
+      await page.goto('https://blog.bouzuya.net/2018/01/01/related/');
+      return {};
+    },
+    name: 'entry-index/1024x768'
   }
 ];
 
