6ffcc07a0d5347fab9d9c2771c0af8608f86b861
bouzuya <m@bouzuya.net>
2018-01-10 11:42:57 +0900

    add scenario and fix some bugs

--- test/scenario/index.ts
+++ test/scenario/index.ts
@@ -1,6 +1,15 @@
 import { Scenario } from '@bouzuya/screenshot-testing/lib/data/scenario';
+import { Page } from 'puppeteer';
 
 const scenarios: Scenario[] = [
+  {
+    action: async (page: Page) => {
+      page.setViewport({ height: 480, width: 320 });
+      page.goto('https://blog.bouzuya.net/2016/01/01/');
+      return {};
+    },
+    name: 'entry-index/320x480'
+  }
 ];
 
 export { scenarios };
